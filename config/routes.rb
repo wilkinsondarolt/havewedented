55d320e8835742ed372ebc9d725d1b028080fd45
Wilkinson Da Rolt de Souza <wilki.darolt@gmail.com>
2024-04-14 18:43:34 -0300

    Alter to use turboframe correctly.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   resources :players, only: [:index]
+  resources :games, only: [:index]
   resources :search, only: [:index]
 
   # Defines the root path route ("/")
