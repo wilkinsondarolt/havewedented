4feb6ab23a926a5aaffc9faf335ded414e9b913d
Wilkinson Da Rolt de Souza <wilki.darolt@gmail.com>
2024-04-16 17:01:20 -0300

    Refactor aoeworld cache implementation.

--- app/services/aoeworld.rb
+++ app/services/aoeworld.rb
@@ -4,15 +4,17 @@ class Aoeworld
   AOE4WORLD_URL = 'https://aoe4world.com'
   AOE4WORLD_API_URL = "#{AOE4WORLD_URL}/api/v0/"
 
+  AOE4WORLD_SHORT_CACHE_TIME = 1.minute
+  AOE4WORLD_LONG_CACHE_TIME = 1.minute
+
   def player(id)
     return {} unless id.positive?
 
-    player_info = Rails.cache.fetch("player_#{id}_data", expires_in: 1.day) do
-      url = "players/#{id}"
-      response = connection.get(url)
-
-      response.success? ? response.body : ""
-    end
+    player_info = cached_response(
+      url: "players/#{id}",
+      cache_key: "player_#{id}_data",
+      expires_in: AOE4WORLD_LONG_CACHE_TIME
+    )
 
     return {} if player_info.empty?
 
@@ -22,12 +24,11 @@ class Aoeworld
   def players(name)
     return [] if name.blank?
 
-    players_data = Rails.cache.fetch("players_#{name}_data", expires_in: 1.minute) do
-      url = "players/search?query=#{name}"
-      response = connection.get(url)
-
-      response.success? ? response.body : ""
-    end
+    players_data = cached_response(
+      url: "players/search?query=#{name}",
+      cache_key: "players_#{name}_data",
+      expires_in: AOE4WORLD_LONG_CACHE_TIME
+    )
 
     return [] if players_data.empty?
 
@@ -35,12 +36,11 @@ class Aoeworld
   end
 
   def games(player_id:, opponent_id:)
-    games_data = Rails.cache.fetch("player_#{player_id}_opponent_#{opponent_id}_games", expires_in: 1.minute) do
-      url = "players/#{player_id}/games?opponent_profile_id=#{opponent_id}"
-      response = connection.get(url)
-
-      response.success? ? response.body : ""
-    end
+    games_data = cached_response(
+      url: "players/#{player_id}/games?opponent_profile_id=#{opponent_id}",
+      cache_key: "player_#{player_id}_opponent_#{opponent_id}_games",
+      expires_in: AOE4WORLD_SHORT_CACHE_TIME
+    )
 
     return [] if games_data.empty?
 
@@ -51,5 +51,13 @@ class Aoeworld
 
   def connection
     @connection ||= Faraday.new(url: AOE4WORLD_API_URL)
+  end
+
+  def cached_response(url:, cache_key:, expires_in:)
+    Rails.cache.fetch(cache_key, expires_in: expires_in) do
+      response = connection.get(url)
+
+      response.success? ? response.body : ""
+    end
   end
 end
