abf2ed76dba904c97e302c34d9acb3472e28ff76
Wilkinson Da Rolt de Souza <wilki.darolt@gmail.com>
2024-04-16 19:58:31 -0300

    Add tom-select to the project.

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,12 +2,23 @@ class PlayersController < ApplicationController
   def index
     name = permitted_params[:name]
 
-    render json: Aoeworld.instance.players(name)
+    render json:search_players(name)
   end
 
   private
 
   def permitted_params
     params.permit(:name)
+  end
+
+  def search_players(name)
+    aoeworld_users = Aoeworld.instance.players(name)
+
+    aoeworld_users.map do |user|
+      {
+        id: user["profile_id"],
+        name: user['name']
+      }
+    end
   end
 end
